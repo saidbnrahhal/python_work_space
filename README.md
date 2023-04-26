7ab7e4053092b1d425be57d0c592d339a36f238d
Said RAHHAL <saidbnrahhal@gmail.com>
2023-04-26 19:30:36 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# My dockerized Python work space
+Inspired from :
+</br>
+https://github.com/codingforentrepreneurs/Jupyter-x-Docker-on-Heroku </br>
+https://github.com/misohu/python_in_docker</br>
---
+++ src/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+# import debugpy
+
+# debugpy.listen(("0.0.0.0", 5678))
+# print("Waiting for client to attach...")
+# debugpy.wait_for_client()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/status")
+def read_root():
+    
+    return {"tata:": "batata", "foo":"Wa 3fou"}
+
